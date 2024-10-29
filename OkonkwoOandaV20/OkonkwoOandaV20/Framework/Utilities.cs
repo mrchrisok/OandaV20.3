b25a1343a61c1833811576f00658ea7df56fcca2
Osita Okonkwo <mrchrisok@hotmail.com>
2024-10-29 15:41:54 -0500

    add throwIfHalted option

--- OkonkwoOandaV20/OkonkwoOandaV20/Framework/Utilities.cs
+++ OkonkwoOandaV20/OkonkwoOandaV20/Framework/Utilities.cs
@@ -16,7 +16,7 @@ namespace OkonkwoOandaV20.Framework
       /// </summary>
       /// <param name="instrument">Instrument to check if halted. Default is EUR_USD.</param>
       /// <returns>True if trading is halted, false if trading is not halted.</returns>
-      public static async Task<bool> IsMarketHaltedAsync(string instrument = InstrumentName.Currency.EURUSD)
+      public static async Task<bool> IsMarketHaltedAsync(string instrument = InstrumentName.Currency.EURUSD, bool throwIfHalted = false)
       {
          var accountID = Credentials.GetDefaultCredentials().DefaultAccountId;
          var parameters = new PricingParameters() { instruments = new List<string>() { instrument } };
@@ -32,7 +32,13 @@ namespace OkonkwoOandaV20.Framework
             hasAsks = prices[0].asks.Count > 0;
          }
 
-         return !(isTradeable && hasBids && hasAsks);
+         if (isTradeable && hasBids && hasAsks)  // not halted
+            return false;
+
+         if (throwIfHalted)
+            throw new MarketHaltedException($"Market is halted for this instrument: {instrument}");
+
+         return true;
       }
 
       /// <summary>
--- OkonkwoOandaV20/OkonkwoOandaV20/TradeLibrary/Order/REST/GetOrder.cs
+++ OkonkwoOandaV20/OkonkwoOandaV20/TradeLibrary/Order/REST/GetOrder.cs
@@ -10,29 +10,29 @@ namespace OkonkwoOandaV20.TradeLibrary.REST
    /// </summary>
    public partial class Rest20
    {
-	  /// <summary>
-	  /// Retrieves an order belonging to the account
-	  /// </summary>
-	  /// <param name="accountID">the identifier of the account to retrieve the list for</param>
-	  /// <param name="orderSpecifier">The Order Specifier (orderId) [required]</param>
-	  /// <returns>a List of Order objects (or empty list, if no orders)</returns>
-	  public static async Task<IOrder> GetOrderAsync(string accountID, long orderSpecifier, OrderParameters parameters = null)
-	  {
-		 var request = new Request()
-		 {
-			Uri = $"{ServerUri(EServer.Account)}accounts/{accountID}/orders/{orderSpecifier}",
-			Method = "GET",
-			Parameters = parameters ?? new OrderParameters()
-		 };
+      /// <summary>
+      /// Retrieves an order belonging to the account
+      /// </summary>
+      /// <param name="accountID">the identifier of the account to retrieve the list for</param>
+      /// <param name="orderSpecifier">The Order Specifier (orderId) [required]</param>
+      /// <returns>a List of Order objects (or empty list, if no orders)</returns>
+      public static async Task<IOrder> GetOrderAsync(string accountID, long orderSpecifier, OrderParameters parameters = null)
+      {
+         var request = new Request()
+         {
+            Uri = $"{ServerUri(EServer.Account)}accounts/{accountID}/orders/{orderSpecifier}",
+            Method = "GET",
+            Parameters = parameters ?? new OrderParameters()
+         };
 
-		 var response = await MakeRequestAsync<OrderResponse, OrderErrorResponse>(request);
+         var response = await MakeRequestAsync<OrderResponse, OrderErrorResponse>(request);
 
-		 return response.order;
-	  }
+         return response.order;
+      }
 
-	  public class OrderParameters : Parameters
-	  {
-	  }
+      public class OrderParameters : Parameters
+      {
+      }
    }
 
    /// <summary>
@@ -40,8 +40,8 @@ namespace OkonkwoOandaV20.TradeLibrary.REST
    /// </summary>
    public class OrderResponse : Response
    {
-	  [JsonConverter(typeof(OrderConverter))]
-	  public IOrder order { get; set; }
+      [JsonConverter(typeof(OrderConverter))]
+      public IOrder order { get; set; }
    }
 
    /// <summary>
